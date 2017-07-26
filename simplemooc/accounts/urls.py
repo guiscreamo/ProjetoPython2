9d460ffc4d2f0d762e7a6fde8bdd7321e834909b
jgsorio <jgsorio@allnetgroup.com.br>
2017-07-26 15:42:04 -0300

    Modificado dia 26/07/2017 as 1542

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from django.contrib.auth.views import login as authLogin
+from simplemooc.accounts.views import register
 
 urlpatterns = [
     url(r'^$', authLogin, {'template_name': 'accounts/login.html'}, name='login'),
+    url(r'^cadastrar/', register, name='register'),
      
 ]
